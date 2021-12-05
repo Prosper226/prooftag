96da075280f7b60fa27947a26465446f10e0917e
Prosper226 <psedgo@gmail.com>
2021-12-05 13:17:18 +0000

    updated

--- icivil/api/users/GET.php
+++ icivil/api/users/GET.php
@@ -1,7 +1,7 @@
 <?php
    // session_start();
 
-   require_once "../../../vendor/autoload.php";
+   // require_once "../../../vendor/autoload.php";
    require_once "../vendor/autoload.php";
 
    use GuzzleHttp\Client;
